d9591d4271e7c494d2dc2bd582429f93e074b753
Sowmys18 <98528062+Sowmys18@users.noreply.github.com>
2023-02-21 18:14:37 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './admin/home/home.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { SidebarComponent } from './admin/sidebar/sidebar.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { PostsComponent } from './admin/dashboard/posts/posts.component';
+import { CommentsComponent } from './admin/dashboard/comments/comments.component';
+import { PagesComponent } from './admin/dashboard/pages/pages.component';
+import { SettingsComponent } from './admin/dashboard/settings/settings.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    DashboardComponent,
+    PostsComponent,
+    CommentsComponent,
+    PagesComponent,
+    SettingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
